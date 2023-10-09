f8a723db676a5cfbcd5988bf72ef941955925a98
Jaeyeong Shin <87981867+jaeyeongs@users.noreply.github.com>
2023-10-09 17:33:00 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33212 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,12 @@
+word = input().upper()
+word_list = list(set(word))
+word_count = []
+
+for i in word_list:
+    count = word.count(i)
+    word_count.append(count)
+
+if word_count.count(max(word_count)) >= 2:
+    print("?")
+else:
+    print(word_list[word_count.index(max(word_count))])
