81be5f3f31b8576bd93a7306ab40c7f1df91b9d0
Jaeyeong Shin <87981867+jaeyeongs@users.noreply.github.com>
2023-10-20 22:05:46 +0900

    [level 1] Title: 폰켓몬, Time: 0.39 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,4 +1,4 @@
 def solution(nums):
-    choice = len(nums) // 2
-    pokemon = list(set(nums))
+    choice = len(nums) // 2 # 선택 수는 nums의 절반
+    pokemon = list(set(nums)) # 같은 종류의 포켓몬을 set으로 중복 제거
     return min(len(pokemon), choice)
