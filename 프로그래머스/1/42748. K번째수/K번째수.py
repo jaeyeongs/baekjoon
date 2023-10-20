5b4f39979e40245c9899eea4c568f0fc1574bc4b
Jaeyeong Shin <87981867+jaeyeongs@users.noreply.github.com>
2023-10-20 21:42:13 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,9 @@
 def solution(array, commands):
     answer = []
+    
+    # i, j, k에 2차원 배열의 원소 담기
     for i, j, k in commands:
-        a = sorted(array[i-1:j])
-        answer.append(a[k-1])
+        a = sorted(array[i-1:j]) # i-1:j 까지 슬라이싱하고 오름차순 나열
+        answer.append(a[k-1]) # k-1번째 원소 추가
+        
     return answer
