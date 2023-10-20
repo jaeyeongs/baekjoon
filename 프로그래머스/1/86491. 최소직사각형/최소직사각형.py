196c1b26d3b9b495e2e6fce12a74d34a7418a8ad
Jaeyeong Shin <87981867+jaeyeongs@users.noreply.github.com>
2023-10-20 21:00:23 +0900

    [level 1] Title: 최소직사각형, Time: 7.32 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,13 +1,14 @@
 def solution(sizes):
-    max_len = []
-    min_len = []
+    max_len = [] # 최대 길이
+    min_len = [] # 최소 길이
 
     for w, h in sizes:
-        if max(w, h):
+        if max(w, h): # 가로, 세로 중 최대값
             max_len.append(max(w, h))
-        if min(w, h):
+        if min(w, h): # 가로, 세로 중 최솟값
             min_len.append(min(w, h))
 
+    # 명함의 가로, 세로 길이 중 최대값과 가로, 세로 중 작은 값 중의 최대값
     result = max(max_len) * max(min_len)
 
     return result
