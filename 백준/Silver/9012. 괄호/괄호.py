c769c0db8fd077162c81618bd8a0fc7a590597a9
Jaeyeong Shin <87981867+jaeyeongs@users.noreply.github.com>
2023-10-27 23:08:44 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+for _ in range(T):
+    lst = list(input())
+    sum = 0
+
+    for i in lst:
+        if i == "(":
+            sum += 1
+        elif i == ")":
+            sum -= 1
+        if sum < 0:
+            print("NO")
+            break
+            
+    if sum > 0:
+        print("NO")
+    elif sum == 0:
+        print("YES")
