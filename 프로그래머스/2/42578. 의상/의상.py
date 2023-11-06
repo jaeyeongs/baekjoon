0e3767a35eebb8ea64b71ad9046c65adb805d78a
Jaeyeong Shin <87981867+jaeyeongs@users.noreply.github.com>
2023-11-06 15:08:11 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -10,3 +10,18 @@ def solution(clothes):
         answer *= n+1
 
     return answer-1
+
+# def solution(clothes):
+#     clothes_type = {}
+
+#     for c, t in clothes:
+#         if t not in clothes_type:
+#             clothes_type[t] = 2
+#         else:
+#             clothes_type[t] += 1
+
+#     cnt = 1
+#     for num in clothes_type.values():
+#         cnt *= num
+
+#     return cnt - 1
